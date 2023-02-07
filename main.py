fa3e4d2660aa0b76287186378761e1a92890457f
sfeldman1 <122839625+sfeldman1@users.noreply.github.com>
2023-02-07 03:47:55 +0000

    completed steps 1, 2, 3

--- main.py
+++ main.py
@@ -3,12 +3,19 @@ CMPS 2200  Recitation 2
 """
 
 ### the only imports needed are here
-import tabulate
+#import tabulate
 import time
 ###
 
 def simple_work_calc(n, a, b):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + n
+  if n == 1:
+    return 1
+  else:
+    return a*simple_work_calc(n//b, a, b ) + n
+    
+    
+  
+    """Compute the value of the recurrence $W(n) = aW(n/b) + n
 
 	Params:
 	n......input integer
@@ -17,17 +24,29 @@ def simple_work_calc(n, a, b):
 
 	Returns: the value of W(n).
 	"""
+  
+  
+  
 	# TODO
-	pass
+
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	
+  assert simple_work_calc(10, 2, 2) == 36
+  assert simple_work_calc(20, 3, 2) == 230
+  assert simple_work_calc(30, 4, 2) == 650
+  assert simple_work_calc(12, 5, 3) == 57
+  assert simple_work_calc(40, 1, 2) == 78
+  assert simple_work_calc(100, 2, 10) == 124
 
 def work_calc(n, a, b, f):
-	"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
+  if n == 1:
+    return 1
+  else:
+    return a*work_calc(n//b, a, b, f) + f(n)
+
+  
+"""Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
 
 	Params:
 	n......input integer
@@ -39,7 +58,7 @@ def work_calc(n, a, b, f):
 	Returns: the value of W(n).
 	"""
 	# TODO
-	pass
+
 
 def span_calc(n, a, b, f):
 	"""Compute the span associated with the recurrence $W(n) = aW(n/b) + f(n)
@@ -57,10 +76,13 @@ def span_calc(n, a, b, f):
 	pass
 
 def test_work():
-	""" done. """
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+  
+  assert work_calc(10, 2, 2, lambda n: 1) == 15
+  assert work_calc(20, 1, 2, lambda n: n*n) == 530
+  assert work_calc(30, 3, 2, lambda n: n) == 300
+  assert work_calc(12, 5, 3, lambda n: 3) == 43
+  assert work_calc(40, 1, 2, lambda n: n) == 78
+  assert work_calc(100, 2, 10, lambda n: n*n) == 10204
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
@@ -97,7 +119,7 @@ def test_compare_work():
 	# create work_fn2
 
     res = compare_work(work_fn1, work_fn2)
-	print(res)
+    print(res)
 
 def test_compare_span():
-	# TODO
+	"TODO"
--- test_main.py
+++ test_main.py
@@ -1,12 +1,24 @@
 from main import *
 
 def test_simple_work():
-	""" done. """
-	assert work_calc(10, 2, 2) == #TODO
-	assert work_calc(20, 3, 2) == #TODO
-	assert work_calc(30, 4, 2) == #TODO
+	
+  assert simple_work_calc(10, 2, 2) == 36
+  assert simple_work_calc(20, 3, 2) == 230
+  assert simple_work_calc(30, 4, 2) == 650
+  assert simple_work_calc(12, 5, 3) == 57
+  assert simple_work_calc(40, 1, 2) == 78
+  assert simple_work_calc(100, 2, 10) == 124
+
+
+
+
+
+
 
 def test_work():
-	assert work_calc(10, 2, 2,lambda n: 1) == #TODO
-	assert work_calc(20, 1, 2, lambda n: n*n) == #TODO
-	assert work_calc(30, 3, 2, lambda n: n) == #TODO
+  assert work_calc(10, 2, 2,lambda n: 1) == 15
+  assert work_calc(20, 1, 2, lambda n: n*n) == 530
+  assert work_calc(30, 3, 2, lambda n: n) == 300
+  assert work_calc(12, 5, 3, lambda n: 3) == 43
+  assert work_calc(40, 1, 2, lambda n: n) == 78
+  assert work_calc(100, 2, 10, lambda n: n*n) == 10204
