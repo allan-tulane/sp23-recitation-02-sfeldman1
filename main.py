59751beffe8d3d3ab864c6ef3dbcf3322e8ff555
sfeldman1 <122839625+sfeldman1@users.noreply.github.com>
2023-02-08 03:48:36 +0000

    fixed 1,2,3

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import time
 ###
 
 def simple_work_calc(n, a, b):
-  if n == 1:
+  if n <= 1:
     return 1
   else:
-    return a*simple_work_calc(n//b, a, b ) + n
+    return a*simple_work_calc(n/b, a, b ) + n
     
     
   
@@ -32,18 +32,18 @@ def simple_work_calc(n, a, b):
 
 def test_simple_work():
 	
-  assert simple_work_calc(10, 2, 2) == 36
-  assert simple_work_calc(20, 3, 2) == 230
-  assert simple_work_calc(30, 4, 2) == 650
-  assert simple_work_calc(12, 5, 3) == 57
-  assert simple_work_calc(40, 1, 2) == 78
-  assert simple_work_calc(100, 2, 10) == 124
+  assert simple_work_calc(10, 2, 2) == 56.0
+  assert simple_work_calc(20, 3, 2) == 506.75
+  assert simple_work_calc(30, 4, 2) == 1954.0
+  assert simple_work_calc(12, 5, 4) == 52.0
+  assert simple_work_calc(40, 1, 2) == 79.75
+  assert simple_work_calc(100, 2, 10) == 124.0
 
 def work_calc(n, a, b, f):
-  if n == 1:
+  if n <= 1:
     return 1
   else:
-    return a*work_calc(n//b, a, b, f) + f(n)
+    return a*work_calc(n/b, a, b, f) + f(n)
 
   
 """Compute the value of the recurrence $W(n) = aW(n/b) + f(n)
@@ -77,12 +77,12 @@ def span_calc(n, a, b, f):
 
 def test_work():
   
-  assert work_calc(10, 2, 2, lambda n: 1) == 15
-  assert work_calc(20, 1, 2, lambda n: n*n) == 530
-  assert work_calc(30, 3, 2, lambda n: n) == 300
-  assert work_calc(12, 5, 3, lambda n: 3) == 43
-  assert work_calc(40, 1, 2, lambda n: n) == 78
-  assert work_calc(100, 2, 10, lambda n: n*n) == 10204
+  assert work_calc(10, 2, 2, lambda n: 1) == 31
+  assert work_calc(20, 1, 2, lambda n: n*n) == 533.8125
+  assert work_calc(30, 3, 2, lambda n: n) == 638.625
+  assert work_calc(12, 5, 3, lambda n: 3) == 218
+  assert work_calc(40, 1, 2, lambda n: n) == 79.75
+  assert work_calc(100, 2, 10, lambda n: n*n) == 10204.0
 
 def compare_work(work_fn1, work_fn2, sizes=[10, 20, 50, 100, 1000, 5000, 10000]):
 	"""
--- test_main.py
+++ test_main.py
@@ -2,12 +2,12 @@ from main import *
 
 def test_simple_work():
 	
-  assert simple_work_calc(10, 2, 2) == 36
-  assert simple_work_calc(20, 3, 2) == 230
-  assert simple_work_calc(30, 4, 2) == 650
-  assert simple_work_calc(12, 5, 3) == 57
-  assert simple_work_calc(40, 1, 2) == 78
-  assert simple_work_calc(100, 2, 10) == 124
+  assert simple_work_calc(10, 2, 2) == 56.0
+  assert simple_work_calc(20, 3, 2) == 506.75
+  assert simple_work_calc(30, 4, 2) == 1954.0
+  assert simple_work_calc(12, 5, 3) == 52.0
+  assert simple_work_calc(40, 1, 2) == 79.95
+  assert simple_work_calc(100, 2, 10) == 124.0
 
 
 
@@ -16,9 +16,9 @@ def test_simple_work():
 
 
 def test_work():
-  assert work_calc(10, 2, 2,lambda n: 1) == 15
-  assert work_calc(20, 1, 2, lambda n: n*n) == 530
-  assert work_calc(30, 3, 2, lambda n: n) == 300
-  assert work_calc(12, 5, 3, lambda n: 3) == 43
-  assert work_calc(40, 1, 2, lambda n: n) == 78
-  assert work_calc(100, 2, 10, lambda n: n*n) == 10204
+  assert work_calc(10, 2, 2,lambda n: 1) == 31
+  assert work_calc(20, 1, 2, lambda n: n*n) == 533.8125
+  assert work_calc(30, 3, 2, lambda n: n) == 638.625
+  assert work_calc(12, 5, 3, lambda n: 3) == 218
+  assert work_calc(40, 1, 2, lambda n: n) == 79.75
+  assert work_calc(100, 2, 10, lambda n: n*n) == 10204.0
